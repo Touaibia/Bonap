7f6cb8924d2fab095f9372a8609bf3a730e1b239
MNour7 <mahamadou.nour7@gmail.com>
2018-01-29 10:53:00 +0100

    defintion des types de publication

--- app/src/main/java/com/bonapp/ujm/myapplication/GestionPublication.java
+++ app/src/main/java/com/bonapp/ujm/myapplication/GestionPublication.java
@@ -3,6 +3,10 @@ package com.bonapp.ujm.myapplication;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by maham on 23/01/2018.
@@ -15,6 +19,17 @@ public class GestionPublication extends MenuManagerActivity {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
 
+        Spinner spinner = (Spinner) findViewById(R.id.selectType);
+
+        spinner.setOnItemSelectedListener(selectItem);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.type_pub, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
         RecyclerView rv = (RecyclerView) findViewById(R.id.list);
         rv.setLayoutManager(new LinearLayoutManager(this));
         rv.setAdapter(new PublicationAdapter());
@@ -23,4 +38,16 @@ public class GestionPublication extends MenuManagerActivity {
     protected  int getLayoutId(){
         return R.layout.gestion_publications;
     }
+
+    AdapterView.OnItemSelectedListener selectItem = new AdapterView.OnItemSelectedListener() {
+
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
+    };
 }
