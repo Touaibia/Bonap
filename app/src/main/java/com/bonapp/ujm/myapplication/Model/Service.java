2b5d4dcc51e6d4649a400660623dd0c2c2990575
DavyUVD <desgarduria@gmail.com>
2018-03-05 11:01:46 +0100

    [add] class Service used for reservation

---
+++ app/src/main/java/com/bonapp/ujm/myapplication/Model/Service.java
@@ -0,0 +1,23 @@
+package com.bonapp.ujm.myapplication.Model;
+
+/**
+ * Created by Davy on 05/03/2018.
+ */
+
+public class Service {
+    int service;
+    Date date;
+
+    public Service(int s, Date d){
+        this.service = s;
+        this.date = d;
+    }
+
+    public int getService() {
+        return service;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+}
