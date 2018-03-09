5fd180c149da0fb075d60ccf96f58addf7261065
touaibia <adel_231994@hotmail.fr>
2018-03-09 17:28:45 +0100

    [Revision des interfaces inscription et leur foncctionnement avec la base de donnes ]

--- app/src/main/java/com/bonapp/ujm/myapplication/Controller/clientregister.java
+++ app/src/main/java/com/bonapp/ujm/myapplication/Controller/clientregister.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.bonapp.ujm.myapplication.Model.Adresse;
 import com.bonapp.ujm.myapplication.Model.Client;
 import com.bonapp.ujm.myapplication.Model.RepoInscription;
 import com.bonapp.ujm.myapplication.R;
@@ -18,14 +20,15 @@ public class clientregister extends AppCompatActivity implements View.OnClickLis
 
      Databasehelper  helper = new Databasehelper(this);
 
-        EditText username,email,password,confirmpassword,adress,ville,telephone;
+        EditText username,email,password,confirmpassword,adress,ville,telephone,numero, codePostal, lebeller, ClientPhone;
+    Spinner typeVoix;
         Button Register;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_clientregister);
 
-            Register = (Button) findViewById(R.id.clientRegister);
+            Register = (Button) findViewById(R.id.registerClient);
             Register.setOnClickListener(this);
 
         }
@@ -33,23 +36,36 @@ public class clientregister extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.clientRegister:
+            case R.id.registerClient:
 
                 username = (EditText) findViewById(R.id.clientPseudo);
                 password = (EditText) findViewById(R.id.clientPassword);
                 confirmpassword = (EditText) findViewById(R.id.clientConfirmPswrd);
                 email = (EditText) findViewById(R.id.clientEmail);
-                adress = (EditText) findViewById(R.id.clientadress);
+              /*  adress = (EditText) findViewById(R.id.clientadress);
                 ville = (EditText) findViewById(R.id.Clienville);
-                telephone = (EditText) findViewById(R.id.clientPhone);
+                telephone = (EditText) findViewById(R.id.clientPhone);*/
+                numero = (EditText) findViewById(R.id.ClientNumeRue);
+                typeVoix = (Spinner)findViewById(R.id.ClientTypeVoix);
+                lebeller = (EditText) findViewById(R.id.ClientLebel);
+                codePostal = (EditText) findViewById(R.id.ClientCodePostal);
+                ClientPhone = (EditText) findViewById(R.id.ClientPhone);
 
                 String userName = username.getText().toString();
                 String passWord = password.getText().toString();
                 String confirmPassword = confirmpassword.getText().toString();
                 String Email = email.getText().toString();
-                String Adress = adress.getText().toString();
+              /*  String Adress = adress.getText().toString();
                 String villle = ville.getText().toString();
-                String Telephone = telephone.getText().toString();
+                String Telephone = telephone.getText().toString();*/
+                String NumRue = numero.getText().toString();
+                String NomRue = lebeller.getText().toString();
+                String CodPostal = codePostal.getText().toString();
+                String CleintTel = ClientPhone.getText().toString();
+                Adresse ClientAdress = new  Adresse( NumRue, "boulvard",NomRue, CodPostal);
+
+
+                //  String TypeDeVoix = typeVoix.getText()
 
                 if(!passWord.equals(confirmPassword)){
                     Toast pass = Toast.makeText(clientregister.this," Password don't match",Toast.LENGTH_SHORT);
@@ -62,9 +78,11 @@ public class clientregister extends AppCompatActivity implements View.OnClickLis
                         user.setUsername(userName);
                         user.setEmail(Email);
                         user.setPassword(passWord);
-                        user.setAdress(Adress);
+                        user.setAdresse(ClientAdress);
+                    /*    user.setAdress(Adress);
                         user.setCity(villle);
-                        user.setPhone(Telephone);
+                        user.setPhone(Telephone);*/
+                        user.setPhone(CleintTel);
                     RepoInscription repoInscription = new RepoInscription(this);
                     repoInscription.insertContact(user);
 
